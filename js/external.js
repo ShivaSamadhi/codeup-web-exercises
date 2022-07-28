3583b95f6a3405e891b5ca1bb7dfc482b1851c00
Ramaj Johnson <rjohnson3795@gmail.com>
2022-07-28 14:22:48 -0500

    functional storage

--- js/external.js
+++ js/external.js
@@ -3,32 +3,45 @@ alert('Welcome to my Website!');
 let answer = prompt(`What's your favorite color?`);
 alert(`Awesome, ${answer} is my favorite color too!`);
 
-let lM = prompt('How many days would you like to rent Little Mermaid?');
-let bB = prompt('How many days would you like to rent Brother Bear?');
-let herc = prompt('How many days would you like to rent Hercules?');
-let price = 3;
-let total = (lM*price)+(bB*price)+(herc*price);
-alert(`Your total rental price is: $${total}`);
+let rental = function () {
+    let lM = prompt('How many days would you like to rent Little Mermaid?');
+    let bB = prompt('How many days would you like to rent Brother Bear?');
+    let herc = prompt('How many days would you like to rent Hercules?');
+    let price = 3;
+    let total = (lM*price)+(bB*price)+(herc*price);
+    alert(`Your total rental price is: $${total}`);
+};
+rental();
 
-alert(`Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350.`);
-let google = prompt(`How many hours do you work for Google a week?`)
-let amazon = prompt(`How many hours do you work for Amazon a week?`)
-let face = prompt(`How many hours do you work for Facebook a week?`)
+let payDay = function () {
+    alert(`Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350.`);
+    let google = prompt(`How many hours do you work for Google a week?`)
+    let amazon = prompt(`How many hours do you work for Amazon a week?`)
+    let face = prompt(`How many hours do you work for Facebook a week?`)
 
-let gPay = 400
-let aPay = 380
-let fPay = 350
-let weeklyWage = (google*gPay)+(amazon*aPay)+(face*fPay)
-alert(`Your gross weekly income is: $${weeklyWage}`)
+    let gPay = 400
+    let aPay = 380
+    let fPay = 350
+    let weeklyWage = (google*gPay)+(amazon*aPay)+(face*fPay)
+    alert(`Your gross weekly income is: $${weeklyWage}`)
+};
+payDay();
 
-let enroll = confirm(`Would you like to enroll in this class?`)
-let isFull = confirm(`Click OKAY if the class is not already full`)
-let isConflict = confirm(`Click OKAY if the class does not conflict with your current schedule`)
-alert(`Your enrollment status is: ${enroll && isFull && isConflict}`)
+let enrollment = function () {
+    let enroll = confirm(`Would you like to enroll in this class?`)
+    let isFull = confirm(`Click OKAY if the class is not already full`)
+    let isConflict = confirm(`Click OKAY if the class does not conflict with your current schedule`)
+    alert(`Your enrollment status is: ${enroll && isFull && isConflict}`)
+};
+enrollment()
 
-alert(`You may be eligible for a special offer`)
-let premium = confirm(`Click OKAY if you are a premium member`)
-let items = prompt(`How many items do you have in your cart?`)
-let twoOrMore = items >= 2
-alert(`Do you qualify for the offer: ${premium || twoOrMore}`)
+let offer = function () {
+    alert(`You may be eligible for a special offer`)
+    let premium = confirm(`Click OKAY if you are a premium member`)
+    let items = prompt(`How many items do you have in your cart?`)
+    let twoOrMore = items >= 2
+    alert(`Do you qualify for the offer: ${premium || twoOrMore}`)
+};
+enrollment()
+
 console.log('Hello from external JavaScript');
