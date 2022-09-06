aab6ce02d740cac8c5659db12d77c4b8fc019f7c
Ramaj Johnson <rjohnson3795@gmail.com>
2022-09-06 13:04:29 -0500

    console log removed

--- js/jquery-faq.js
+++ js/jquery-faq.js
@@ -3,7 +3,6 @@ $(document).ready(function (){
     $('dd').addClass('invisible')
     $("#toggle").click(function(event) {
         event.preventDefault();
-        console.log("this was executed")
         $("dd").toggleClass('invisible')
     })
 });
