1dca68570a057660f8381de54bcdad1e1160c462
Ramaj Johnson <rjohnson3795@gmail.com>
2022-09-07 10:41:20 -0500

    facts added

--- js/jquery-faq.js
+++ js/jquery-faq.js
@@ -1,13 +1,33 @@
 "use strict"
 $(document).ready(function (){
-    $('dd').addClass('invisible')
+    $('dd').addClass('invisible');
+
     $("#toggle").click(function(event) {
         event.preventDefault();
         $("dd").toggleClass('invisible')
-    })
+    });
+
     $('dt').click(function () {
         $(this).css('background-color', "red")
     }).on('dblclick', function () {
         $(this).css('background-color', 'white')
+    });
+
+    $('.color-change').click(function () {
+        $('ul').children().css('background-color', 'goldenrod')
+    }).on('dblclick', function () {
+        $('ul').children().css('background-color', 'white')
+    })
+    
+    $('.np-name').on('click', function () {
+        $(this).next().children().css('font-weight', 'bold')
+    }).on('dblclick', function () {
+        $(this).next().children().css('font-weight', 'normal')
+    })
+
+    $('li').on('click', function () {
+        $(this).parent().children().first().css('color', 'blue')
+    }).on('dblclick', function () {
+        $(this).parent().children().first().css('color', 'black')
     })
 });
