b9fd4a0de2f4c247d3606a9bdfac15bb2fa9e498
Ramaj Johnson <rjohnson3795@gmail.com>
2022-09-09 11:46:20 -0500

    ajax store refactor

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -5,14 +5,18 @@
     $.get(blog).done(function (posts) {
         posts.forEach(post => {
             $('#posts').append(
-                `<div class="col-6">
-                 <h1 class="">${post.title}</h1>
-                 <h5>${post.date}</h5> 
-                 <p>${post.content}</p>
-                 </div>`
+                `<div class="card mb-5 border-dark d-flex" style="width: 100%;">
+                  <div class="card-body bg-dark bg-opacity-75">
+                    <h3 class="card-title font-2 text-light">${post.title}</h3>
+                    <h6 class="card-subtitle mb-2 text-dark">${post.date}</h6>
+                    <p class="card-text font-2 text-light">${post.content}</p>
+                    <p class="card-text text-dark mt-1">${post.categories}</p>
+                  </div>
+                </div>`
             )
         })
     })
+    $('body').css("background-image", "url('../img/sunset-at-the-ocean-wallpaper-1920x1080-wallpaper.jpeg')")
 })();
 
 
