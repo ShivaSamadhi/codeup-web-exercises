f0814185d8fae46e68effe34a0c6d19a234560b8
Ramaj Johnson <rjohnson3795@gmail.com>
2022-09-09 12:08:49 -0500

    background image refactor

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -2,10 +2,23 @@
     "use strict"
     const blog = '../data/blog.json'
 
-    $.get(blog).done(function (posts) {
+    function update() {
+        $.get(blog).done(function(posts) {
+            let postCreate = blogHTML(posts); // create HTML string
+            $('#posts').html(postCreate); // update the DOM with the new HTML
+        }).fail(function(jqXhr, status, error) {
+            console.log(jqXhr);
+            console.log(status);
+            console.log(error);
+        });
+    }
+    update();
+    
+    function blogHTML(posts) {
+        let html = ''
         posts.forEach(post => {
-            $('#posts').append(
-                `<div class="card mb-5 border-dark d-flex" style="width: 100%;">
+            html +=
+                `<div class="card mb-3 border-dark d-flex" style="width: 100%;">
                   <div class="card-body bg-dark bg-opacity-75">
                     <h3 class="card-title font-2 text-light">${post.title}</h3>
                     <h6 class="card-subtitle mb-2 text-dark">${post.date}</h6>
@@ -13,10 +26,15 @@
                     <p class="card-text text-dark mt-1">${post.categories}</p>
                   </div>
                 </div>`
-            )
+
         })
-    })
-    $('body').css("background-image", "url('../img/sunset-at-the-ocean-wallpaper-1920x1080-wallpaper.jpeg')")
+        return html
+    }
+
+    setInterval(function() {
+        update();
+    }, 3000);
+
 })();
 
 
