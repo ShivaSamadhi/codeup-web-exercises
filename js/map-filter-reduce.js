67d183385145dac7abc50ce17bbd7f4bf1300bb6
Ramaj Johnson <rjohnson3795@gmail.com>
2022-09-16 10:10:13 -0500

    filter map reduce

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,52 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+let multiLang = users.filter(user => user.languages.length >= 3)
+console.log(multiLang)
+let emails = users.map(({email}) => email)
+console.log(emails)
+let experience = users.reduce((total,{yearsOfExperience})=>{
+    return (total + yearsOfExperience)
+}, 0)
+let avg = (total) => {
+    let length = users.length
+    return total/length
+}
+console.log(avg(experience))
+console.log(experience)
+console.log(avg())
