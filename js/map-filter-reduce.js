1e902e4d894344f180dc4d3a117f0cef4bf2c223
Ramaj Johnson <rjohnson3795@gmail.com>
2022-09-16 10:41:31 -0500

    map filter reduce complete

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -38,15 +38,25 @@ const users = [
 
 let multiLang = users.filter(user => user.languages.length >= 3)
 console.log(multiLang)
+
 let emails = users.map(({email}) => email)
 console.log(emails)
+
 let experience = users.reduce((total,{yearsOfExperience})=>{
     return (total + yearsOfExperience)
 }, 0)
+
 let avg = (total) => {
     let length = users.length
     return total/length
 }
+let longEmail = users.reduce((arr, {email})=>{
+    arr.push(email);
+    let max = arr[0].length;
+    arr.map(item => max = Math.max(max, item.length));
+    let str = arr.filter(item => item.length == max);
+    return str;
+},[])
 console.log(avg(experience))
 console.log(experience)
-console.log(avg())
+console.log(longEmail)
